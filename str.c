681cd768fa91da0c43924b4692d8ff83f9340976
adut24 <dutertre.alexandre@laposte.net>
2022-04-08 17:02:20 +0200

    Adding strcmp

--- str.c
+++ str.c
@@ -7,24 +7,24 @@
  */
 int _strlen(char *s)
 {
-	int length = 0;
+	int i = 0;
 
-	while (s[length] != '\0')
-		length++;
-	return (length);
+	while (s[i] != '\0')
+		i++;
+	return (i);
 }
 
 /**
- * *_strcpy - copy a string
+ * _strcpy - copy a string
  * @dest: destination of the string
  * @src: source of the string
  * Return: string to copy
  */
 char *_strcpy(char *dest, char *src)
 {
-	int i, length = _strlen(src);
+	int i, len = _strlen(src);
 
-	for (i = 0; i <= length; i++)
+	for (i = 0; i <= len; i++)
 	{
 		dest[i] = src[i];
 	}
@@ -32,38 +32,38 @@ char *_strcpy(char *dest, char *src)
 }
 
 /**
- * *_strchr - locate a character in a string
- * @s: string to verify
- * @c: character to check if present
- * Return: pointer to the position of c or 0
+ * _strcmp - compare two strings
+ * @s1: string one
+ * @s2: string two
+ * Return: gap between the ASCII value of the two characters
  */
-char *_strchr(char *s, char c)
+int _strcmp(char *s1, char *s2)
 {
-	for (; *s; s++)
+	while (*s1 == *s2)
 	{
-		if (*s == c)
-			break;
+		if (*s1 == '\0')
+			return (0);
+		s1++;
+		s2++;
 	}
-	if (!*s && c)
-		return (0);
-	return (s);
+	return (*s1 - *s2);
 }
 
 /**
- * *_strcat - concatenate 2 strings together
+ * _strcat - concatenate 2 strings together
  * @dest: the string receiving the other
  * @src: the string to concatenate
  * Return: concatenated string
  */
 char *_strcat(char *dest, char *src)
 {
-	int i, length = _strlen(dest);
+	int i, len = _strlen(dest);
 
 	for (i = 0; src[i] != '\0' ; i++)
 	{
-		dest[length + i] = src[i];
+		dest[len + i] = src[i];
 	}
-	dest[length + i] = '\0';
+	dest[len + i] = '\0';
 	return (dest);
 }
 
@@ -75,16 +75,14 @@ char *_strcat(char *dest, char *src)
  */
 char *_strdup(char *str)
 {
-	int i, length = _strlen(str);
-	char *s = malloc(length * sizeof(char) + 1);
+	int i, len = _strlen(str);
+	char *s;
 
+	s = malloc((len + 1) * sizeof(char));
 	if (s == NULL)
 		return (NULL);
-	for (i = 0; i < length; i++)
-	{
+	for (i = 0; i < len; i++, str++)
 		s[i] = *str;
-		str++;
-	}
 	s[i] = '\0';
 	return (s);
 }
